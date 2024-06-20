eb046b2ad3b0710421a1674b8a287e829e74c4ef
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-06-20 16:38:35 +0900

    Title: 최고의 13위치, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240620/최고의 13위치/best-place-of-13.py
+++ 240620/최고의 13위치/best-place-of-13.py
@@ -1,11 +1,13 @@
 n = int(input())
-mat = [list(map(int, input().split())) for _ in range(3)]
+mat = [list(map(int, input().split())) for _ in range(n)]
 
 mx = 0
-for i in range(n - 2):
-    cnt = 0
-    for j in range(i, i + 3):
-        if mat[i][j] == 1:
-            cnt += 1
-    mx = max(mx, cnt)
-print(cnt)
+for i in range(n):
+    for j in range(n - 2):
+        cnt = 0
+        for k in range(j, j + 3):
+            print(i, k , cnt)
+            if mat[i][k] == 1:
+                cnt += 1
+        mx = max(mx, cnt)
+print(mx)
