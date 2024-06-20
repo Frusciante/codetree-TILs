e5a9ce798a299f80703d75c5be18a97f906c782a
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-06-20 16:39:02 +0900

    Title: 최고의 13위치, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/최고의 13위치/best-place-of-13.py
+++ 240620/최고의 13위치/best-place-of-13.py
@@ -6,7 +6,6 @@ for i in range(n):
     for j in range(n - 2):
         cnt = 0
         for k in range(j, j + 3):
-            print(i, k , cnt)
             if mat[i][k] == 1:
                 cnt += 1
         mx = max(mx, cnt)
