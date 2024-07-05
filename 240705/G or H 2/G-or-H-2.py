61db1a92f6386a8c92223dacdd7929e03593b838
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-05 15:12:20 +0900

    Title: G or H 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240705/G or H 2/G-or-H-2.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+arr = []
+for _ in range(n):
+    pos, alpha = input().split()
+    pos = int(pos)
+    arr.append((pos, alpha))
+arr.sort()
+
+max_all = 0
+for i in range(n-1):
+    max_part = 0
+    cnt_g = 0
+    cnt_h = 0
+    if arr[i][1] == 'G':
+        cnt_g += 1
+    else:
+        cnt_h += 1
+    for j in range(i+1, n):
+        if arr[j][1] == 'G':
+            cnt_g += 1
+        else:
+            cnt_h += 1
+        if cnt_g == cnt_h:
+            max_part = arr[j][0] - arr[i][0]
+    max_all = max(max_all, max_part)
+
+print(max_all)
