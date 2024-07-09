9730c9f3dc6efbdc3137674467248a93c0e24127
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-09 17:18:51 +0900

    Title: 개발팀의 능력, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240709/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240709/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -17,8 +17,11 @@ for i in range(5):
                 team1 = arr[i] + arr[j]
                 team2 = arr[k] + arr[l]
                 team3 = s - team1 - team2
-                if team1 == team2 == team3:
+                if team1 == team2 or team1 == team3 or team2 == team3:
                     continue
                 min_val = min(max(team1, team2, team3) - min(team1, team2, team3), min_val)
 
+if min_val == sys.maxsize:
+    min_val = -1
+    
 print(min_val)
