822621ebb816c303a5bda51c1c1af01c65ecbd49
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-09 17:13:13 +0900

    Title: 개발팀의 능력, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240709/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,24 @@
+arr = list(map(int, input().split()))
+s = sum(arr)
+
+import sys
+min_val = sys.maxsize
+
+for i in range(5):
+    for j in range(5):
+        if i == j:
+            continue
+        for k in range(5):
+            if j == k or k == i:
+                continue
+            for l in range(5):
+                if l == k or l == j or l == i:
+                    continue
+                team1 = arr[i] + arr[j]
+                team2 = arr[k] + arr[l]
+                team3 = s - team1 - team2
+                if team1 == team2 == team3:
+                    continue
+                min_val = min(max(team1, team2, team3) - min(team1, team2, team3), min_val)
+
+print(min_val)
