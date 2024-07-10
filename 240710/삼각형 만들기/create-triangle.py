7963237c18241f59fee2929a1a9aa783f0ca4248
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-10 11:39:01 +0900

    Title: 삼각형 만들기, Time: 244ms, Memory: 25MB, Status: Passed - CodeTree

--- 240710/삼각형 만들기/create-triangle.py
+++ 240710/삼각형 만들기/create-triangle.py
@@ -9,12 +9,8 @@ for i in range(n):
         if x1 == x2:
             for k in range(n):
                 x3, y3 = points[k]
-                w = abs((y1 - y2) * (x1 - x3))
-                max_w = max(w, max_w)
-        if y1 == y2:
-            for k in range(n):
-                x3, y3 = points[k]
-                w = abs((x1 - x2) * (y1 - y3))
-                max_w = max(w, max_w)
+                if y1 == y3 or y1 == y2:
+                    w = abs((y1 - y2) * (x1 - x3))
+                    max_w = max(w, max_w)
 
 print(max_w)
