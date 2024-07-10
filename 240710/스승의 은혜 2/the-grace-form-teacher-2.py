358f1d10ac5ec6dfa7e76954b33f1784bcfd17bb
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-10 17:13:50 +0900

    Title: 스승의 은혜 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240710/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,18 @@
+n, b = tuple(map(int, input().split()))
+arr = [int(input()) for _ in range(n)]
+
+arr.sort()
+
+cnt = 0
+ans = 0
+for i in range(n):
+    if ans + arr[i] < b:
+        ans += arr[i]
+        cnt += 1
+    elif ans + (arr[i] // 2) < b:
+        ans += arr[i] // 2
+        cnt += 1
+        break
+    else:
+        break
+print(cnt)
