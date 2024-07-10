54ae796c035cf63be52fc343415f6385e5d8f6c5
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-10 17:16:10 +0900

    Title: 스승의 은혜 2, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240710/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240710/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -6,10 +6,10 @@ arr.sort()
 cnt = 0
 ans = 0
 for i in range(n):
-    if ans + arr[i] < b:
+    if ans + arr[i] <= b:
         ans += arr[i]
         cnt += 1
-    elif ans + (arr[i] // 2) < b:
+    elif ans + (arr[i] // 2) <= b:
         ans += arr[i] // 2
         cnt += 1
         break
