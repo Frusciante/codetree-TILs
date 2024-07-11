194a91708e0016ce9d4b02b7a15757999fe1fc0f
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-11 17:22:04 +0900

    Title: 스승의 은혜 3, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240711/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240711/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,7 +1,7 @@
 n, b = tuple(map(int, input().split()))
 arr = [tuple(map(int, input().split())) for _ in range(n)]
 
-arr.sort(key = lambda x: (x[0] + x[1], -x[0]))
+arr.sort(key = lambda x: (x[0] + x[1], x[1]))
 
 cnt = 0
 ans = 0
