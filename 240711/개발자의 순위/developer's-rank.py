93636635588cbe215bdd686a05a2b84f135d5d44
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-11 14:59:00 +0900

    Title: 개발자의 순위, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/개발자의 순위/developer's-rank.py
@@ -0,0 +1,20 @@
+k, n = tuple(map(int, input().split()))
+games = [list(map(int, input().split())) for _ in range(k)]
+all_noc = [[] for _ in range(k)]
+
+for i in range(k):
+    for j in range(n):
+        for l in range(j + 1, n):
+            all_noc[i].append((games[i][j], games[i][l]))
+
+cnt = 0
+for item in games[0]:
+    check = True
+    for j in range(1, k):
+        if item not in games[j]:
+            check = False
+            break
+    if check:
+        cnt += 1
+
+print(cnt)
