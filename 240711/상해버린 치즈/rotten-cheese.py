c0056dcdbf79971bc8df8375849c30503ff06a89
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-11 11:53:51 +0900

    Title: 상해버린 치즈, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240711/상해버린 치즈/rotten-cheese.py
+++ 240711/상해버린 치즈/rotten-cheese.py
@@ -2,3 +2,24 @@ n, m, d, s = tuple(map(int, input().split()))
 
 eat_rec = [tuple(map(int, input().split())) for _ in range(d)]
 ach_rec = [tuple(map(int, input().split())) for _ in range(s)]
+eat_rec.sort(key = lambda x: (x[0], x[2]))
+
+possible = [0] * (m + 1)
+for (patient, start) in ach_rec:
+    for (person, cheese, time) in eat_rec:
+        if patient != person:
+            continue
+        if time < start:
+            possible[cheese] += 1
+        else:
+            break
+
+max_medicine = 0
+for i in range(1, m + 1):
+    cnt = 0
+    for (person, cheese, time) in eat_rec:
+        if cheese == possible[i]:
+            cnt += 1
+    max_medicine = max(max_medicine, cnt)
+
+print(max_medicine)
