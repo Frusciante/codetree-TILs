5145ddea79af8f60b0d640c9c08827316f8caa14
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-11 17:51:37 +0900

    Title: 스승의 은혜 3, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240711/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240711/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -2,7 +2,6 @@ n, b = tuple(map(int, input().split()))
 arr = [tuple(map(int, input().split())) for _ in range(n)]
 
 arr.sort(key = lambda x: (x[0] + x[1], x[0]))
-
 cnt = 0
 ans = 0
 for (price, shipping) in arr:
@@ -12,6 +11,4 @@ for (price, shipping) in arr:
     elif ans + (price // 2) + shipping <= b:
         ans += (price // 2) + shipping
         cnt += 1
-    else:
-        break
 print(cnt)
