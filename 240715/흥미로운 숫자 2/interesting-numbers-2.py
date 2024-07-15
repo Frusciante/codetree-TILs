13fdf8bff62a787d107e98a2d05422c5b3791320
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-15 10:24:48 +0900

    Title: 흥미로운 숫자 2, Time: 192ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240715/흥미로운 숫자 2/interesting-numbers-2.py
@@ -3,17 +3,24 @@ x, y = tuple(map(int, input().split()))
 cnt = 0
 for i in range(x, y + 1):
     num_str = str(i)
+    len_str = len(num_str)
     first = num_str[0]
-    second = num_str[1]
+
+    another = -1
+    for n in num_str:
+        if n != first:
+            another = n
+            break
     
-    check = True
-    num_cnt = 0
+    f_cnt = 0
+    a_cnt = 0
     for n in num_str:
         if n == first:
-            num_cnt += 1
+            f_cnt += 1
+        elif n == another:
+            a_cnt += 1
     
-    if (num_cnt == 1 and num_str[1] == num_str[2]) or (len(num_str) - 1) == num_cnt:
+    if (a_cnt == 1 or f_cnt == 1) and (a_cnt + f_cnt == len_str):
         cnt += 1
-    
 
 print(cnt)
