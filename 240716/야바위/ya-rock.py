aa6132345927751639437d4d251db5af71802826
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-16 17:45:23 +0900

    Title: 야바위, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/야바위/ya-rock.py
+++ 240716/야바위/ya-rock.py
@@ -1,15 +1,17 @@
 n = int(input())
 actions = [tuple(map(int, input().split())) for _ in range(n)]
+cups = [0] * 4
 
 high_score = 0
 for i in range(1, 4):
     score = 0
-    cups = [0] * 4
     cups[i] = 1
     for a, b, c in actions:
         cups[a], cups[b] = cups[b], cups[a]
         if cups[c] == 1:
             score += 1
     high_score = max(score, high_score)
+    for j in range(1, 4):
+        cups[j] = 0
 
 print(high_score)
