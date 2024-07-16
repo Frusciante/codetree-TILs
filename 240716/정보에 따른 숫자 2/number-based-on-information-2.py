0a13684f961f0f6eec1c2daf5156acf17101851a
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-16 15:22:44 +0900

    Title: 정보에 따른 숫자 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240716/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,40 +1,24 @@
+import sys
+
 t, a, b = tuple(map(int, input().split()))
 line = [0] * (1001)
 
+data = []
 for _ in range(t):
     c, num = tuple(input().split())
     num = int(num)
-    line[num] = c
+    data.append((c, num))
+
 cnt = 0
 for i in range(a, b + 1):
-    d1 = 0
-    d2 = 0
-    d1_found = False
-    d2_found = False
-    move = 0
-    while (i - move >= 0) or (i + move <= 1000):
-        if i - move >= 0 and line[i - move] != 0:
-            if line[i - move] == 'S':
-                if not d1_found:
-                    d1 = move
-                    d1_found = True
-            if line[i - move] == 'N':
-                if not d2_found:
-                    d2 = move
-                    d2_found = True
-        if i + move <= 1000 and line[i + move] != 0:
-            if line[i + move] == 'S':
-                if not d1_found:
-                    d1 = move
-                    d1_found = True
-            if line[i + move] == 'N':
-                if not d2_found:
-                    d2 = move
-                    d2_found = True
-        if d1_found and d2_found:
-            break
-        move += 1
-    if d1 <= d2 and d1_found and d2_found:
+    d1 = sys.maxsize
+    d2 = sys.maxsize
+    for item in data:
+        c, num = item
+        if c == 'S':
+            d1 = min(d1, abs(i - num))
+        else:
+            d2 = min(d2, abs(i - num))
+    if d1 <= d2:
         cnt += 1
-
 print(cnt)
