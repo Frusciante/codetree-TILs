87cdbf42635167192b54b086333b7df11803176c
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-16 16:44:11 +0900

    Title: 등차수열, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/등차수열/arithmetic-sequence.py
+++ 240716/등차수열/arithmetic-sequence.py
@@ -2,11 +2,13 @@ t = int(input())
 arr = list(map(int, input().split()))
 biggest = max(arr)
 
-cnt = 0
-for i in range(t - 1):
-    for j in range(i + 1, t):
-        for k in range(2, biggest):
-            if ((arr[i] + arr[j])) / 2 == k:
+max_cnt = 0
+for k in range(2, biggest):
+    cnt = 0
+    for i in range(t - 1):
+        for j in range(i + 1, t):
+            if (arr[i] + arr[j]) == 2 * k:
                 cnt += 1
+    max_cnt = max(max_cnt, cnt)
 
-print(cnt)
+print(max_cnt)
