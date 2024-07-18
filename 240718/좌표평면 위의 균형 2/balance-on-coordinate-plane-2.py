641870dc8088f4bfa84522eaae75096eb122d253
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-18 15:44:54 +0900

    Title: 좌표평면 위의 균형 2, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240718/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 240718/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -16,9 +16,14 @@ for i in range(n):
     maxy = max(maxy, y)
     points.append((x, y))
 
+minx = minx - 1 if minx % 2 == 1 else minx
+maxx = maxx + 1 if maxx % 2 == 1 else maxx
+miny = miny - 1 if miny % 2 == 1 else miny
+maxy = maxy + 1 if maxy % 2 == 1 else maxy
+
 minm = n
-for i in range(minx, maxx + 1):
-    for j in range(miny, maxy + 1):
+for i in range(minx, maxx + 1, 2):
+    for j in range(miny, maxy + 1, 2):
         cnts = [0] * 4
         for x, y in points:
             if x > i and y > j:
@@ -29,7 +34,9 @@ for i in range(minx, maxx + 1):
                 cnts[2] += 1
             else:
                 cnts[3] += 1
-        
         minm = min(max(cnts), minm)
+
+        for k in range(4):
+            cnts[k] = 0
 
 print(minm)
