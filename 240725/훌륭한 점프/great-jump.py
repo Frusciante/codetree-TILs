84f5103f21c17d11ad4fd26cdbba908809e0f643
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-25 14:52:26 +0900

    Title: 훌륭한 점프, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/훌륭한 점프/great-jump.py
+++ 240725/훌륭한 점프/great-jump.py
@@ -1,12 +1,13 @@
 n, k = tuple(map(int, input().split()))
 stones = list(map(int, input().split()))
-middle_stones = stones[1:n-1]
 
 def is_possible(max_val):
     available_indices = []
     for i, stone in enumerate(stones):
         if stone <= max_val:
             available_indices.append(i)
+    if 0 not in available_indices:
+        return False
     for j in range(1, len(available_indices)):
         if available_indices[j] - available_indices[j-1] > k:
             return False
@@ -14,8 +15,8 @@ def is_possible(max_val):
 
 minmax = 100
 #중간에 있는 돌들을 각각 최대값이라고 가정하고 순회
-for item in middle_stones:
+for item in stones:
     if is_possible(item):
         minmax = min(minmax, item)
 
-print(item)
+print(minmax)
