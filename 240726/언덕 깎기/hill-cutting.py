0bbfb64e632502aad89075f0ced68d24c9aa6bf0
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-26 13:44:18 +0900

    Title: 언덕 깎기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240726/언덕 깎기/hill-cutting.py
+++ 240726/언덕 깎기/hill-cutting.py
@@ -1,5 +1,6 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
+cnt_arr = [0] * n
 
 def update(arr):
     maxval = max(arr)
@@ -9,15 +10,15 @@ def update(arr):
     return maxval, minval, maxidx, minidx
 
 maxval, minval, maxidx, minidx = update(arr)
-add_cnt = 0
-minus_cnt = 0
 while maxval - minval > 17:
     arr[minidx] += 1
-    add_cnt += 1
+    cnt_arr[minidx] += 1
     if maxval - minval <= 17:
         break
     arr[maxidx] -= 1
-    minus_cnt += 1
+    cnt_arr[minidx] += 1
+
     maxval, minval, maxidx, minidx = update(arr)
 
-print(minus_cnt ** 2 + add_cnt ** 2)
+print(maxval, minval, maxidx, minidx, arr)
+print(cnt_arr)
