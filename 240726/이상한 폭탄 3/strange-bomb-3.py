02191cc8ca29c81a4e4107a2ec075d44e5f43a5b
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-26 11:13:31 +0900

    Title: 이상한 폭탄 3, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,22 @@
+n, k = tuple(map(int, input().split()))
+bombs = [int(input()) for _ in range(n)]
+bombs_cnt = {bomb: 0 for bomb in bombs}
+
+for i, bomb in enumerate(bombs):
+    for j in range(1, bomb + 1):
+        if i + j >= n:
+            break
+        if bombs[i + j] == bomb:
+            bombs_cnt[bomb] += 2 if bombs_cnt[bomb] == 0 else 1
+            break
+
+maximum = 0
+max_bomb_num = 0
+for bomb_num in bombs_cnt:
+    if bombs_cnt[bomb_num] > maximum:
+        maximum = bombs_cnt[bomb_num]
+        max_bomb_num = bomb_num
+    elif bombs_cnt[bomb_num] == maximum and bomb_num > max_bomb_num:
+        max_bomb_num = bomb_num
+
+print(max_bomb_num)
