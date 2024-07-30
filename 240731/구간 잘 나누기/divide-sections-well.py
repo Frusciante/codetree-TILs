7b873d4e097a63ba52daf1a1cdf9c8d769f2c57a
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-31 01:09:11 +0900

    Title: 구간 잘 나누기, Time: 239ms, Memory: 28MB, Status: Passed - CodeTree

--- 240731/구간 잘 나누기/divide-sections-well.py
+++ 240731/구간 잘 나누기/divide-sections-well.py
@@ -18,7 +18,9 @@ for i in range(10000):
             sec += 1
 
         cnt += arr[j]
-
+    
+    # 합의 상한 정했고, 넘으면 섹션 추가했으므로 섹션 수는 m보다 작을 수 있음.
+    # 나눌 수 있는 부분 아무렇게나 나누면 됨.
     if possible and m >= sec:
         print(i)
         break
