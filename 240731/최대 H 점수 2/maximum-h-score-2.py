01629e58f9a686611d62bef26a72f2b1a5dcf895
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-31 17:41:51 +0900

    Title: 최대 H 점수 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240731/최대 H 점수 2/maximum-h-score-2.py
+++ 240731/최대 H 점수 2/maximum-h-score-2.py
@@ -1,4 +1,4 @@
-# H 점수값 순회 --> 가능한 값인가 확인 필요
+# H 점수값 순회 --> 가능한 값인가 확인 필요, H점수는 n을 넘지 못함
 n, l = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
@@ -6,10 +6,11 @@ result = 0
 for i in range(n + 1):
     cnt = 0
     cntl = 0
-    # n보다 작은 수 개수
     for j in range(n):
+        # H값과 원소 값 비교
         if arr[j] >= i:
             cnt += 1
+        #같다면 세기 시작, l 이상인지 확인
         elif arr[j] == i - 1:
             if cntl < l:
                 cntl += 1
