f1ab716e8a262e56531d87706e6d7a701846d81e
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-07-31 16:04:55 +0900

    Title: 최대 H 점수 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240731/최대 H 점수 2/maximum-h-score-2.py
+++ 240731/최대 H 점수 2/maximum-h-score-2.py
@@ -1,35 +1,21 @@
 # H 점수값 순회 --> 가능한 값인가 확인 필요
-
-def cal_hscore(nums):
-    maxim = 0
-    for times in nums:
-        cnt = 0
-        for num in nums:
-            if num >= times:
-                cnt += 1       
-        if cnt >= times:
-            maxim = max(maxim, times)
-    return maxim
-
 n, l = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-cnt_arr = [0] * 101
-for num in arr:
-    for i in range(101):
-        if i <= num:
-            cnt_arr[i] += 1
-            
-start = cal_hscore(arr)
-m = start
-for h in range(start, 101):
-    c = arr.count(h)
-    if l >= c:
-        if (h + 1) in arr:
-            if c + cnt_arr[h + 1] >= h + 1:
-                m = h + 1
-        else:
-            if cnt_arr[h] >= h + 1:
-                m = h + 1
+result = 0
+for i in range(n + 1):
+    cnt = 0
+    cntl = 0
+    # n보다 작은 수 개수
+    for j in range(n):
+        if arr[j] >= i:
+            cnt += 1
+        elif arr[j] == i - 1:
+            if cntl < l:
+                cntl += 1
+                cnt += 1
+    
+    if cnt >= i:
+        result = i
 
-print(m)
+print(result)
