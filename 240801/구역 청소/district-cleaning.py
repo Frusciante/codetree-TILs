026b4242f83c548f3e3ab4dffd801c6eab510123
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-08-01 13:37:44 +0900

    Title: 구역 청소, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/구역 청소/district-cleaning.py
+++ 240801/구역 청소/district-cleaning.py
@@ -1,11 +1,7 @@
 a1, a2 = tuple(map(int, input().split()))
 b1, b2 = tuple(map(int, input().split()))
-start = min(a1, b1)
-end = max(a2, b2)
-areas = [0] * (end + 1)
 
-for i in range(start, end):
-    if (a1 <= i and i < a2) or (b1 <= i and i < b2):
-        areas[i] = 1
-
-print(sum(areas))
+if a2 < b1 or b2 < a1:
+    print((a2 - a1) + (b2 - b1))
+else:
+    print(max(a2, b2) - min(a1, b1))
