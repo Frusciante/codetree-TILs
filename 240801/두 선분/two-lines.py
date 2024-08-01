9581394c1205bc5e344a697a6e8cef2f15932108
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-08-01 11:28:55 +0900

    Title: 두 선분, Time: 208ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/두 선분/two-lines.py
+++ 240801/두 선분/two-lines.py
@@ -1,6 +1,6 @@
 x1, x2, x3, x4 = tuple(map(int, input().split()))
 
-if (x3 <= x1 and x1 <= x4) or (x3 <= x2 and x2 <= x4) or (x1 <= x3 and x3 <= x2) or (x1 <= x4 and x4 <= x2):
-    print('intersecting')
-else:
+if x2 < x3 or x4 < x1:
     print('nonintersecting')
+else:
+    print('intersecting')
