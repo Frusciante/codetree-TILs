4f9c2deb66f0c25440306cc433e7e034fba51025
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-08-01 13:22:17 +0900

    Title: 구역 청소, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/구역 청소/district-cleaning.py
+++ 240801/구역 청소/district-cleaning.py
@@ -5,7 +5,7 @@ end = max(a2, b2)
 areas = [0] * (end + 1)
 
 for i in range(start, end):
-    if (a1 <= i and i <= a2) or (b1 <= i and i <= b2):
+    if (a1 <= i and i < a2) or (b1 <= i and i < b2):
         areas[i] = 1
 
 print(sum(areas))
