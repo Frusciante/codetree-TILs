76838170be57b086f877c1e3627349d4450821c6
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-08-01 15:18:46 +0900

    Title: 전부 겹치는 선분 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240801/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240801/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,18 +1,19 @@
 import sys
 n = int(input())
-arr = [tuple(map(int, input().split()))]
+arr = [tuple(map(int, input().split())) for _ in range(n)]
 
 for i in range(n):
-    mini = sys.maxsize
-    maxi = -sys.maxsize
-
+    min_x2 = sys.maxsize
+    max_x1 = -sys.maxsize
+    overlapped = True
     for j, (x1, x2) in enumerate(arr):
         if i == j:
             continue
-        mini = min(x1, mini)
-        maxi = max(x2, maxi)
-
-    if mini >= maxi:
+        max_x1 = max(x1, max_x1)
+        min_x2 = min(x2, min_x2)
+    if max_x1 > min_x2:
+        overlapped = False
+    if overlapped:
         print('Yes')
         break
 else:
