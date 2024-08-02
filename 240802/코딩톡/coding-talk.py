ba4f62aa8af39bc1e34d0154b132373eabaee6c1
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-08-02 17:28:28 +0900

    Title: 코딩톡, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/코딩톡/coding-talk.py
+++ 240802/코딩톡/coding-talk.py
@@ -1,7 +1,7 @@
 n, m, p = tuple(map(int, input().split()))
 c_record = []
 u_record = []
-for i in range(n):
+for i in range(m):
     c, u = input().split()
     c_record.append(c)
     u_record.append(int(u))
@@ -10,12 +10,12 @@ full = [chr(65 + i) for i in range(n)]
 
 while p >= 1 and u_record[p - 1] == u_record[p - 2]:
     p -= 1
-    
+
 if u_record[p - 1] == 0:
     print("")
 else:
     answer = set()
-    for i in range(p - 1, n):
+    for i in range(p - 1, m):
         answer.add(c_record[i])
 
     answer = list(answer)
