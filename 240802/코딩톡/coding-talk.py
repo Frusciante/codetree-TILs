7147e98373e9ff7af3e6ba8bf7064939b8ef07b5
Frusciante <149743835+Frusciante@users.noreply.github.com>
2024-08-02 17:27:05 +0900

    Title: 코딩톡, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240802/코딩톡/coding-talk.py
@@ -0,0 +1,24 @@
+n, m, p = tuple(map(int, input().split()))
+c_record = []
+u_record = []
+for i in range(n):
+    c, u = input().split()
+    c_record.append(c)
+    u_record.append(int(u))
+
+full = [chr(65 + i) for i in range(n)]
+
+while p >= 1 and u_record[p - 1] == u_record[p - 2]:
+    p -= 1
+    
+if u_record[p - 1] == 0:
+    print("")
+else:
+    answer = set()
+    for i in range(p - 1, n):
+        answer.add(c_record[i])
+
+    answer = list(answer)
+    for c in full:
+        if c not in answer:
+            print(c, end=' ')
